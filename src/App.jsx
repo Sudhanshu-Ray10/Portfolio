d8d44fdd5a834d54afd764ded8848a7c6d17828f
Sudhanshu Ray <sudhanshray10@gmail.com>
2025-11-10 18:09:51 +0530

    inital push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <>
+  <div className='text-red-600 text-9xl font-bold'>Sudhsnshu is a good boy.
+  </div>
+  
+  </>
+  )
+}
+
+export default App
